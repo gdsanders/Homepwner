c35197b1e5bf7fe6bd0696af26480b340b88746e
gdsanders <gd@digitalequity.com>
2015-06-20 21:15:52 -0500

    Initial Commit

---
+++ Homepwner/DEIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEIAppDelegate.h
+//  Homepwner
+//
+//  Created by G.D. Sanders on 6/20/15.
+//  Copyright (c) 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
