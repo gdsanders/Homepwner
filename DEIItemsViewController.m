6090442169d72045bb67b67235f6916652d37acc
gdsanders <gd@digitalequity.com>
2015-06-20 21:22:47 -0500

    Override initWithStyle initializer

--- DEIItemsViewController.m
+++ DEIItemsViewController.m
@@ -10,4 +10,16 @@
 
 @implementation DEIItemsViewController
 
+- (instancetype)init
+{
+    // Call the superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
 @end
