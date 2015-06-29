9d9d087004ae163847d2586eee71cf72c21ca8bb
gdsanders <gd@digitalequity.com>
2015-06-29 18:05:04 -0500

    DetailViewController added

---
+++ Homepwner/DEIDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DEIDetailViewController.h
+//  Homepwner
+//
+//  Created by G.D. Sanders on 6/29/15.
+//  Copyright (c) 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEIDetailViewController : UIViewController
+
+@end
---
+++ Homepwner/DEIDetailViewController.m
@@ -0,0 +1,18 @@
+//
+//  DEIDetailViewController.m
+//  Homepwner
+//
+//  Created by G.D. Sanders on 6/29/15.
+//  Copyright (c) 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import "DEIDetailViewController.h"
+
+@interface DEIDetailViewController ()
+
+@end
+
+@implementation DEIDetailViewController
+
+
+@end
