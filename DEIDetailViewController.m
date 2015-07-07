b8dd7f21b297cbad56e6722988ba11d61c9f0824
gdsanders <gd@digitalequity.com>
2015-07-06 22:48:18 -0500

    DEIDetailViewController added

--- DEIDetailViewController.m
+++ DEIDetailViewController.m
@@ -2,7 +2,7 @@
 //  DEIDetailViewController.m
 //  Homepwner
 //
-//  Created by G.D. Sanders on 6/29/15.
+//  Created by G.D. Sanders on 7/6/15.
 //  Copyright (c) 2015 DigitalEquity, Inc. All rights reserved.
 //
 
