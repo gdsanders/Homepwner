488f97deebbc84854af5280c23870cbcfb5d062e
gdsanders <gd@digitalequity.com>
2015-07-09 16:50:58 -0500

    XIB changes; added Fabric

--- DEIDetailViewController.m
+++ DEIDetailViewController.m
@@ -9,6 +9,10 @@
 #import "DEIDetailViewController.h"
 
 @interface DEIDetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 
 @end
 
